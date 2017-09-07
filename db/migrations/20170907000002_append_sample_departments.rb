59aca6fa70a36dbe009afb09c364e10cee91608c
Hiroyuki.Nagata <hiroyuki.nagata@veeva.com>
2017-09-07 16:40:22 +0900

    Add test DDL files

---
+++ db/migrations/20170907000002_append_sample_departments.rb
@@ -0,0 +1,14 @@
+--
+-- SQLs from https://www.ntu.edu.sg/home/ehchua/programming/sql/SampleDatabases.html
+--
+INSERT INTO `departments` VALUES
+('d001','Marketing'),
+('d002','Finance'),
+('d003','Human Resources'),
+('d004','Production'),
+('d005','Development'),
+('d006','Quality Management'),
+('d007','Sales'),
+('d008','Research'),
+('d009','Customer Service');
+
