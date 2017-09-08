8c0e981e3ccacc5d5af2e0df4a184d8bdeca2f95
Hiroyuki.Nagata <hiroyuki.nagata@veeva.com>
2017-09-08 10:17:22 +0900

    Add 'api' application for testing

---
+++ apps/api/controllers/employees/list.rb
@@ -0,0 +1,8 @@
+module Api::Controllers::Employees
+  class List
+    include Api::Action
+
+    def call(params)
+    end
+  end
+end
---
+++ apps/api/views/employees/list.rb
@@ -0,0 +1,5 @@
+module Api::Views::Employees
+  class List
+    include Api::View
+  end
+end
